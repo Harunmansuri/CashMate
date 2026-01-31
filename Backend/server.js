e26cbbf971aac910de3ee246f9b917f02a7e64ea
Harun mansuri <mrharun961766@gmail.com>
2026-01-31 10:14:19 +0530

    backend setup

---
+++ Backend/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
+app.use(express.json());
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
